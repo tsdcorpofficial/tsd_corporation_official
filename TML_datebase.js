ca6b3ed5c2005cd654a25cb2bf3044964bf1f572
tsdcorpofficial <tsdcorporationofficial@gmail.com>
2026-06-15 16:22:53 +0300

    Add files via upload

---
+++ TML_datebase.js
@@ -0,0 +1,39 @@
+const Data_news = [{ id: 1, Headers: "У официального сайта TML Official появился свой новостной сервис!" ,text:"У официального сайта TML Official появилась страница с новостями и обновлениями. Теперь вы можете узнать всю нужную информацию об новых версиях и продуктах нашей компании и об их обновлениях", image:"source/images/TOS News Preview 1.png", data:"08.05.2026"}, 
+]
+
+const Data_product = [
+    {id:1, name:"TOS", version:[
+        {name:"TriadeOS 15", download_link:"https://t.me/c/3771689083/17"}, 
+          {name:"TriadeOS 14", download_link:"https://t.me/c/3771689083/4"}, 
+        {name:"TriadeOS 13", download_link:"https://t.me/c/3771689083/5"}, 
+          {name:"TriadeOS 12", download_link:"https://t.me/c/3771689083/6"}, 
+        {name:"TriadeOS 11", download_link:"https://t.me/c/3771689083/7"}
+    ], logo:"source/files/TSD Corporation logo.png"}, 
+    
+    {id:2, name:"SMOS", version:[
+        {name:"SmartfordOS 2", download_link:"https://t.me/c/3771689083/14"}, 
+          {name:"SmartfordOS 1", download_link:"https://t.me/c/3771689083/13"}, 
+        {name:"SmartfordOS beta 1", download_link:""}, 
+    ], logo:"source/files/SmartfordOS logo.png"},
+
+    {id:3, name:"GameSoft", version:[
+        {name:"GameSoft 3", download_link:"https://t.me/c/3771689083/10"}, 
+          {name:"GameSoft 2", download_link:"https://t.me/c/3771689083/9"}, 
+        {name:"GameSoft 1", download_link:"https://t.me/c/3771689083/8"}, 
+    ], logo:"source/files/GameSoft logo.png"},
+
+    {id:4, name:"TriadeX", version:[
+        {name:"TriadeX 1", download_link:"https://t.me/c/3771689083/11"}, 
+    ], logo:"source/files/TriadeX logo.png"}, 
+    
+    {id:5, name:"Trinux", version:[
+        {name:"TriadeOS 15", download_link:"https://t.me/c/3771689083/12"}, 
+    ], logo:"source/files/Trinux logo.png"},
+
+    {id:6, name:"TRcode", version:[
+        {name:"TR code pack 5, 6, 7", download_link:"https://t.me/c/3236619733/106"}, 
+          {name:"TRcode fullpack", download_link:"https://t.me/c/3771689083/16"}, 
+        {name:"TRcode v1", download_link:"https://t.me/c/3771689083/16"}, 
+    ], logo:"source/files/TSD Corporation logo.png"},
+
+]
